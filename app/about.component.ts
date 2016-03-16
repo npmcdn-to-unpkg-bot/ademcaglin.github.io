e35ece58751990449f63c5c73a2157cc5425a062
Adem Caglin <adem.caglin@gmail.com>
2016-03-16 21:38:35 +0200

    Update about.component.ts

--- app/about.component.ts
+++ app/about.component.ts
@@ -1,1 +1,10 @@
+import {Component} from 'angular2/core';
+import {AboutComponent} from './about.component';
 
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html'
+})
+export class AboutComponent {
+  title: 'About mek ds dl dlsd msl dlsj dlksj';
+}
