1d2df09e8e5c6521023e299682fbfc17ce17a6a1
Adem Caglin <adem.caglin@gmail.com>
2016-03-16 21:33:39 +0200

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,10 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {AboutComponent} from './about.component';
 
 @Component({
   selector: 'my-resume',
   templateUrl: './app.component.html',
   directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent {
-  public title = 'Tour of Heroes';
-}
+export class AppComponent { }
