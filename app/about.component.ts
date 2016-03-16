079bae09e590ef2c9f88a4f64ca59e3a73fb2701
Adem Caglin <adem.caglin@gmail.com>
2016-03-16 21:51:00 +0200

    Update about.component.ts

--- app/about.component.ts
+++ app/about.component.ts
@@ -3,7 +3,7 @@ import {AboutComponent} from './about.component';
 
 @Component({
   selector: 'about',
-  templateUrl: './about.component.html'
+  templateUrl: 'app/about.component.html'
 })
 export class AboutComponent {
   title: 'About mek ds dl dlsd msl dlsj dlksj';
